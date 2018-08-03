bb1cd7bf8d4a34c091a370ae313146f35bcb2c14
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2018-08-03 16:54:00 +0800

    [新增] 透過node.js + arduino 完成網頁上繪圖功能，自動將DHT的資料透過串列埠上傳到nodejs伺服器，當使用者請求時進行繪圖

---
+++ webPlotData/webserver_plot_arduino_data/index.js
@@ -0,0 +1,22 @@
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
+const express = require('express');
+const app = express();
+
+// 你的arduino串列埠
+const arduino_port = new SerialPort('COM32');
+// 你的伺服器通訊埠
+const server_port = '3000';
+const parser = arduino_port.pipe(new Readline({ delimiter: '\r\n' }));
+
+var serial_data = [];
+
+parser.on('data', function(data){
+	console.log(data);
+	serial_data.push(data);
+});
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/data', (req, res) => res.send(serial_data));
+
+app.listen(server_port, () => console.log('Example app listening on port', server_port, '!'));
