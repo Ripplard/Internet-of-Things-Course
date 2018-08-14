b6917b716cfd07c524df78076088c688f6df0541
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2018-08-14 18:11:06 +0800

    [修改] 將js的函式庫用靜態檔案綁定，不再使用遠端伺服器的檔案 [新增] 將路由事件多一個/dynamic，可直接導向動態繪圖，方便展示 [新增] 新增js函式庫的檔案(jquery-3.3.1.min.js 、 plotly-1.39.4.min.js)

--- webPlotData/webserver_plot_arduino_data/index.js
+++ webPlotData/webserver_plot_arduino_data/index.js
@@ -16,7 +16,12 @@ parser.on('data', function(data){
 	serial_data.push(data);
 });
 
+app.use('/js',express.static(__dirname + '/js'));
+
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/dynamic', (req, res) => res.sendFile(__dirname + '/index_dynamic.html'));
+
 app.get('/data', (req, res) => res.send(serial_data));
 
 app.listen(server_port, () => console.log('Example app listening on port', server_port, '!'));
