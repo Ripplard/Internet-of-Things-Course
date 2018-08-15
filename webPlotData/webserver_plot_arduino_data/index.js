17bbdde2afd02bb1533320560af89e27b605d332
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2018-08-15 22:21:43 +0800

    [新增] 當成功建立監聽程序時，取得伺服器的IP跟port，並以高亮顯示 [新增] consoleColor.js 檔案提供console的文字顏色更動跳脫符表 [新增] 將html加入title標題 [新增] 將URL /dynamic 直接渲染 index_dynamic.html 檔案

--- webPlotData/webserver_plot_arduino_data/index.js
+++ webPlotData/webserver_plot_arduino_data/index.js
@@ -1,7 +1,11 @@
+const os = require('os');
 const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const express = require('express');
 const app = express();
+
+// 引入console字形跳脫字元顏色表
+const cclor = require('./consoleColor.js');
 
 // 你的arduino串列埠
 const arduino_port = new SerialPort('COM32');
@@ -9,19 +13,54 @@ const arduino_port = new SerialPort('COM32');
 const server_port = '3000';
 const parser = arduino_port.pipe(new Readline({ delimiter: '\r\n' }));
 
+// 串列資料保存陣列
 var serial_data = [];
 
-parser.on('data', function(data){
+// 當取得Arduino串列埠的斷句資料
+parser.on('data', function (data) {
 	console.log(data);
 	serial_data.push(data);
 });
 
-app.use('/js',express.static(__dirname + '/js'));
+// 加載檔案目錄js，並也將它提供到/js的url下
+app.use('/js', express.static(__dirname + '/js'));
 
+// 靜態繪圖URL
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-app.get('/dynamic', (req, res) => res.sendFile(__dirname + '/index_dynamic.html'));
+// 動態繪圖URL
+app.get('/dynamic', (req, res) => {
+	res.sendFile(__dirname + '/index_dynamic.html');
+});
 
+// 用於繪圖取得資料
 app.get('/data', (req, res) => res.send(serial_data));
 
-app.listen(server_port, () => console.log('Example app listening on port', server_port, '!'));
+app.listen(server_port, () => {
+	// 取得網卡IP
+	var IPv4;
+	// 取得網路資訊
+	var ifconfig = os.networkInterfaces();
+
+	// 如有偵測到預設網卡
+	if(ifconfig.hasOwnProperty('乙太網路')) {
+		for (var i = 0; i < ifconfig['乙太網路'].length; i++) {
+			if (ifconfig['乙太網路'][i].family == 'IPv4') {
+				// 取得IP位置
+				IPv4 = ifconfig['乙太網路'][i].address;
+			}
+		}
+		// 印出伺服器實體http的URL
+		console.log(cclor.Reset + cclor.hold + cclor.FgYellow + cclor.hold + cclor.Reset + cclor.hold ,
+					'Example app listening running at \"' ,
+					'http://' + IPv4 + ':' + server_port ,
+					 "\"" );
+	}
+	else{
+		// 印出伺服器的監聽序列埠
+		console.log(cclor.hold + cclor.FgYellow + cclor.hold + cclor.Reset + cclor.hold,
+					'Example app listening running at port \"' ,
+					server_port ,
+					"\"" );
+	}
+});
