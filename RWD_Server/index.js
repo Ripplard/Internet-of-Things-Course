e18189244b7d0eff6c4ed008cc5da81187ceda93
張宏銘 <m10613209@gemail.yuntech.edu.tw>
2018-09-21 00:24:20 +0800

    :hammer: 將靜態檔案直接保存在client端的程序1小時

--- RWD_Server/index.js
+++ RWD_Server/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 const serverIndex = require('serve-index');
-//, { maxAge: 3600000 } 
 
-app.use( express.static( __dirname + '/public'),
+
+app.use( express.static( __dirname + '/public' , { maxAge: 3600000 } ),
 		 serverIndex( __dirname + '/public', { 'icons' : true, 'view' : 'details' } ) );
 app.listen(3005,() => {
     console.log("service on!");
